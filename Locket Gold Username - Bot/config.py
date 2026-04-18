382af76f4b14d173cd7fbadf6365b4580651ea9e
vtuaan2401-coder <vtuaan2401@gmail.com>
2026-04-19 03:54:58 +0700

    Delete Locket Gold Username - Bot directory

--- Locket Gold Username - Bot/config.py
+++
@@ -1,5 +0,0 @@
-import os
-BOT_TOKEN = os.getenv("BOT_TOKEN", "8513671276:AAEmwZbb3Xoj_q2MdEx257HOdAzrVYqNYmc")
-ADMIN_ID = int(os.getenv("ADMIN_ID", "8350416853"))
-QR_IMAGE = os.getenv("QR_IMAGE", "qr.png")
-DATA_FILE = os.getenv("DATA_FILE", "data.json")
